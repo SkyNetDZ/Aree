b128e248433d6b2793fe351f68ce94017e36be47
DESKTOP-9M81EM8\Inneasoft <hedahdia.faycel@outlook.Fr>
2017-01-17 11:59:17 +0100

    indecator style configs

--- src/app/pages/meters/components/chart/performance-chart/performance-chart.component.ts
+++ src/app/pages/meters/components/chart/performance-chart/performance-chart.component.ts
@@ -1,28 +1,45 @@
-import {Component, OnInit, ViewChild, ElementRef, Input, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
   selector: 'app-performance-chart',
   templateUrl: './performance-chart.component.html',
   styleUrls: ['./performance-chart.component.css'],
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.Native
 })
 export class PerformanceChartComponent implements OnInit {
 
   @ViewChild('performancechart') chartContainer: ElementRef;
-  @Input() data: Array<any>;
+  // @Input() data: Array<any>;
   private svg: any;
-  private value: number = 100;
   private y: any;
   private scale: any;
   private targetAnalyse: any;// nomber of columns
   private heightBar: number = 60; // heightBars
   private title: string = 'Chart Title';
   private defaultColor: any;
-  private marginBar = {top: 10, bottom: 10};
+  private marginBar = { top: 10, bottom: 10 };
   private targetMeusureCount: number = 3;
   private offsetAxis: number = 360;
   private indecatorWidth: number = 100;
+  private indecatorData: [number] = [100, 200, 51];
+
+  //Elements of component
+  private table: any;
+  private indecators = new Array<any>();
+  private chart: any;
+
+  //Data
+  private data: Array<any> =
+  [
+    { label: '< 50', max: 180, color: 50, index: 'A' },//[50,55,7]
+    { label: '51-90', max: 200, color: 90, index: 'B' },//[56,61]
+    { label: '91-150', max: 220, color: 150, index: 'C' },
+    { label: '151-230', max: 240, color: 230, index: 'D' },
+    { label: '231-330', max: 260, color: 330, index: 'E' },
+    { label: '331-450', max: 280, color: 450, index: 'F' },
+    { label: '> 450', max: 300, color: 10000, index: 'G' }
+  ];
 
   constructor() {
   }
@@ -42,12 +59,10 @@ export class PerformanceChartComponent implements OnInit {
 
   createChart() {
     this.initSvg();
-    for (var _i = 1; _i < this.targetMeusureCount; _i++) {
-      this.offsetAxis = _i * this.offsetAxis;
-      this.drawIndecator();
-    }
-
+    this.createBars();
+    this.createIndecators();
   }
+
 
   initSvg() {
 
@@ -57,18 +72,92 @@ export class PerformanceChartComponent implements OnInit {
     this.svg = d3.select(element).append('svg')
       .attr('width', 1000)
       .attr('height', 700);
+  }
 
+  createTable() {
+    this.table = this.svg.append('g')
+      .append('table');
+    let thead = this.table.append('thead');
+    let tbody = this.table.append('tbody');
+
+    let columns = ['Title', 'Building', 'Factory'];
+
+    // append the header row
+    thead.append('tr')
+      .selectAll('th')
+      .data(columns).enter()
+      .append('th')
+      .text((column) => column);
+
+    let rows = tbody.append('tr');
+
+    rows.append('td').append('div');
+    rows.selectAll('div').append(this.chart);
+
+    // for (let _i = 0; _i < columns.length; _i++) {
+    //   rows.append('td').append(this.indecators[_i]);
+    // }
+  }
+
+  drawIndecator(value: number) {
+    let indecator = this.svg.append('g');
+    let offsetIndecator = this.offsetAxis + 35;
+    indecator
+      .append('rect')
+      .attr('y', value)
+      .attr('x', offsetIndecator)
+      .style('width', this.indecatorWidth)
+      .style('fill', this.y(value))
+      .style('margin-top', 10)
+      .style('margin-bottom', 10)
+      .style('height', this.heightBar)
+      .style('border', '2px ridge white')
+      .attr('class', 'bar');
+
+    indecator
+      .append('polyline')
+      .attr('y', value)
+      .style('width', 100)
+      .style('fill', this.y(value))
+      .attr('points',
+      offsetIndecator + `,` + (value + 1)
+      + `,` + (offsetIndecator - 30) + `,`
+      + (value + 30) + `,` + offsetIndecator
+      + `,` + (value + 60));
+
+    indecator
+      .append('text')
+      .text(value)
+      .attr('y', value + (this.heightBar / 2))
+      .attr('x', offsetIndecator + 30)
+      .style('font-size', 21)
+      .style('font-weight', 'bold')
+      .attr('color', 'black');
+
+    this.indecators.push(indecator);
+    console.log(this.indecators);
+
+  }
+
+  drawAxis( title: string ) {
+    this.svg.append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(' + this.offsetAxis + ',30)')
+      .call(d3.axisRight(d3.scaleLinear().domain([0, 500]).range([0, 500])).ticks(0))
+      .append('text')
+      .text(title)
+      .style('font-size', 20)
+      .attr('class', 'axis-title')
+      .style('color', 'black');
+      }
+
+  createBars() {
     let domain = [50, 90, 150, 230, 330, 450, 1000];
-
     this.scale = d3.scaleLinear().domain(domain).range([0, 500]);
 
     let generator = d3.scaleLinear()
       .domain([0, (domain.length - 1) / 2, domain.length - 1])
-      .range([
-        'blue',
-        'yellow',
-        'brown']
-      )
+      .range(['green', 'yellow', '#963000'])
       .interpolate(d3.interpolateCubehelix);
 
     let range = d3.range(domain.length).map(generator);
@@ -82,22 +171,12 @@ export class PerformanceChartComponent implements OnInit {
     console.log(d3.quantile(domain, 51));
 
 
-    let maxs = [
-      {label: '< 50', max: 180, color: 50, index: 'A'},//[50,55,7]
-      {label: '51-90', max: 200, color: 90, index: 'B'},//[56,61]
-      {label: '91-150', max: 220, color: 150, index: 'C'},
-      {label: '151-230', max: 240, color: 230, index: 'D'},
-      {label: '231-330', max: 260, color: 330, index: 'E'},
-      {label: '331-450', max: 280, color: 450, index: 'F'},
-      {label: '> 450', max: 300, color: 10000, index: 'G'}
-    ];
-
     let barh = this.svg.append('g')
       .attr('transform', 'translate(10,50)')
       .attr('border', '12px solid black');
 
     let g = barh.selectAll('g')
-      .data(maxs)
+      .data(this.data)
       .enter()
       .append('g');
 
@@ -119,9 +198,9 @@ export class PerformanceChartComponent implements OnInit {
       .style('fill', d => this.y(d.color))
       .style('stroke-width', 3)
       .attr('points', (d, i) =>
-      ((d.max) + ',' + (i * 70) + ',' +
-      (20 + d.max)) + ',' + ((i * 140) + 60) / 2 + ',' +
-      (d.max) + ',' + ((i * 70) + 60));
+        ((d.max) + ',' + (i * 70) + ',' +
+          (20 + d.max)) + ',' + ((i * 140) + 60) / 2 + ',' +
+        (d.max) + ',' + ((i * 70) + 60));
 
     g
       .append('text')
@@ -135,50 +214,17 @@ export class PerformanceChartComponent implements OnInit {
 
     this.svg.append('text')
       .text(this.title)
-      .attr('y', (maxs.length + 1) * 70)
+      .attr('y', (this.data.length + 1) * 70)
       .style('font-size', 20);
 
-    this.svg.append('g')
-      .attr('class', 'axis')
-      .attr('transform', 'translate(' + this.offsetAxis + ',30)')
-      .call(d3.axisRight(d3.scaleLinear().domain([0, 500]).range([0, 500])).ticks(0))
-      .append('text')
-      .text('title')
-      .attr('class', 'axis-title')
-      .attr('text-anchor', 'end');
-
+    this.chart = barh;
   }
 
-  drawIndecator() {
-    let indecator = this.svg.append('g');
-    indecator
-      .append('rect')
-      .attr('y', this.value)
-      .attr('x', this.offsetAxis)
-      .style('width', this.indecatorWidth)
-      .style('fill', this.y(this.value))
-      .style('margin-top', 10)
-      .style('margin-bottom', 10)
-      .style('height', this.heightBar)
-      .style('border', '2px ridge white')
-      .attr('class', 'bar');
-
-    indecator
-      .append('polyline')
-      .attr('y', this.value)
-      .style('width', 100)
-      .style('fill', this.y(this.value))
-      .attr('points', this.offsetAxis + `,` + (this.value + 1) + `,` + (this.offsetAxis - 30) + `,` + (this.value + 30) + `,` + this.offsetAxis + `,` + (this.value + 60));
-
-    indecator
-      .append('text')
-      .text(this.value)
-      .attr('y', this.value + (this.heightBar / 2))
-      .attr('x', 400 + 30)
-      .style('font-size', 21)
-      .style('font-weight', 'bold')
-      .attr('color', 'black');
-
+ createIndecators() {
+    for (let _i = 0; _i < this.targetMeusureCount; _i++) {
+      this.drawAxis('title indecator' + _i);
+      this.drawIndecator(this.indecatorData[_i]);
+      this.offsetAxis = this.offsetAxis + this.indecatorWidth + 50;
+    }
   }
-
 }
